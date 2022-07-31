43a68bf7af649ec046d7358eca73250f46a80337
Gabriel-Tomaz <jgabrieltomaz2706@gmail.com>
2022-07-31 16:24:00 -0300

    iniciando componente global de input e tela de login

---
+++ src/components/input/style.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    inputContainer:{
+        
+    },
+    input: {
+        backgroundColor: '#F8F9FA',
+        paddingHorizontal: 16,
+        paddingVertical: 18,
+        fontSize: 16,
+        color: '#212529',
+        borderWidth: 1,
+        borderRadius: 6,
+        borderColor: '#CED4DA'
+       
+    }
+});
+
+export default styles;
