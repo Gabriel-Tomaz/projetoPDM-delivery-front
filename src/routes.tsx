e2a6aee55fdc566f2675c2901829ac4ba4845dd3
Gabriel-Tomaz <jgabrieltomaz2706@gmail.com>
2022-08-20 11:57:04 -0300

    adiciona a home

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 
+import Home from "./screens/Home";
 import Login from "./screens/login";
 import Product from "./screens/product";
 
@@ -12,8 +13,9 @@ const Routes = () => {
     <NavigationContainer>
       <Drawer.Navigator
         screenOptions={{ headerShown: false }}
-        initialRouteName="Produtos"
+        initialRouteName="Home"
       >
+        <Drawer.Screen name="Home" component={Home} />
         <Drawer.Screen name="Produtos" component={Product} />
         <Drawer.Screen name="Login" component={Login} />
       </Drawer.Navigator>
