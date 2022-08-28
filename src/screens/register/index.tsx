53b9c04df1a82b15ff2661c218d04f9690f479f0
Gabriel-Tomaz <jgabrieltomaz2706@gmail.com>
2022-08-28 02:57:03 -0300

    adiciona componente header

--- src/screens/register/index.tsx
+++ src/screens/register/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View } from "react-native";
+import { View, SafeAreaView, Platform, StatusBar } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import { Formik } from "formik";
 import * as Yup from "yup";
@@ -8,6 +8,7 @@ import Input from "../../components/input";
 import Button from "../../components/button";
 import Colors from "../../styles/colors";
 import { RegisterUser } from "../../types/user";
+import Header from "../../components/header";
 
 const Register = () => {
   const initialValues: RegisterUser = {
@@ -31,67 +32,78 @@ const Register = () => {
   };
 
   return (
-    <KeyboardAwareScrollView
+    <SafeAreaView
       style={{
-        padding: 18,
+        flex: 1,
         backgroundColor: Colors.Neutral.white,
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
       }}
-      resetScrollToCoords={{ x: 0, y: 0 }}
-      contentContainerStyle={{ flex: 1 }}
     >
-      <View>
-        <Formik
-          initialValues={initialValues}
-          validationSchema={SingupSchema}
-          onSubmit={(values: RegisterUser) => Singup(values)}
-        >
-          {({
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            values,
-            touched,
-            errors,
-          }) => (
-            <View>
-              <Input
-                label="Name"
-                placeholder="Digite seu nome"
-                onChange={handleChange("name")}
-                onBlur={handleBlur("name")}
-                value={values.name}
-                error={errors.name && touched.name ? errors.name : undefined}
-              />
-              <Input
-                label="Email"
-                placeholder="Digite seu e-mail"
-                onChange={handleChange("email")}
-                onBlur={handleBlur("email")}
-                value={values.email}
-                error={errors.email && touched.email ? errors.email : undefined}
-              />
-              <Input
-                label="Senha"
-                placeholder="Digite sua senha"
-                password
-                onChange={handleChange("password")}
-                onBlur={handleBlur("password")}
-                value={values.password}
-                error={
-                  errors.password && touched.password
-                    ? errors.password
-                    : undefined
-                }
-              />
+      <Header></Header>
+      <KeyboardAwareScrollView
+        style={{
+          padding: 18,
+          backgroundColor: Colors.Neutral.white,
+        }}
+        resetScrollToCoords={{ x: 0, y: 0 }}
+        contentContainerStyle={{ flex: 1 }}
+      >
+        <View>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={SingupSchema}
+            onSubmit={(values: RegisterUser) => Singup(values)}
+          >
+            {({
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              values,
+              touched,
+              errors,
+            }) => (
+              <View>
+                <Input
+                  label="Name"
+                  placeholder="Digite seu nome"
+                  onChange={handleChange("name")}
+                  onBlur={handleBlur("name")}
+                  value={values.name}
+                  error={errors.name && touched.name ? errors.name : undefined}
+                />
+                <Input
+                  label="Email"
+                  placeholder="Digite seu e-mail"
+                  onChange={handleChange("email")}
+                  onBlur={handleBlur("email")}
+                  value={values.email}
+                  error={
+                    errors.email && touched.email ? errors.email : undefined
+                  }
+                />
+                <Input
+                  label="Senha"
+                  placeholder="Digite sua senha"
+                  password
+                  onChange={handleChange("password")}
+                  onBlur={handleBlur("password")}
+                  value={values.password}
+                  error={
+                    errors.password && touched.password
+                      ? errors.password
+                      : undefined
+                  }
+                />
 
-              <View style={{ marginTop: 8 }}>
-                <Button title="Cadaste-se" onPress={() => handleSubmit()} />
+                <View style={{ marginTop: 8 }}>
+                  <Button title="Cadaste-se" onPress={() => handleSubmit()} />
+                </View>
               </View>
-            </View>
-          )}
-        </Formik>
-      </View>
-    </KeyboardAwareScrollView>
+            )}
+          </Formik>
+        </View>
+      </KeyboardAwareScrollView>
+    </SafeAreaView>
   );
 };
 
