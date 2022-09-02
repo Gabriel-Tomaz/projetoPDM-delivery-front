d49da9d1060177914051aee91f4269fca110ea02
camilacasimiro <camilacasimiiro@gmail.com>
2022-09-01 22:17:05 -0300

    corrigindo conflito

--- src/routes.tsx
+++ src/routes.tsx
@@ -31,8 +31,6 @@ const Routes = () => {
         <Drawer.Screen name="Register" component={Register} />
         <Drawer.Screen name="Profile" component={Profile} />
         <Drawer.Screen name="Cardapio" component={Cardapio} />
-        <Drawer.Screen name="Produtos" component={Product} />
-        <Drawer.Screen name="Login" component={Login} />
         <Drawer.Screen name="OrderHistory" component={OrderHistory} />
         <Drawer.Screen name="OrderDetails" component={OrderDetails} />
         <Drawer.Screen name="ShoppingBag" component={ShoppingBag} />
