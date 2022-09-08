f5ac134aa0d3e2d643fe73488f1c27ed55aa4805
Gabriel-Tomaz <jgabrieltomaz2706@gmail.com>
2022-09-07 23:57:35 -0300

    ajustando home

--- src/screens/cardapio/index.tsx
+++ src/screens/cardapio/index.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import {
   View,
   Text,
-  useWindowDimensions,
   Image,
   FlatList,
   TouchableOpacity,
+  SafeAreaView,
+  Platform,
+  StatusBar,
 } from "react-native";
-import { color } from "react-native-reanimated";
 import Colors from "../../styles/colors";
-import NavBar from "../../components/navBar";
 import styles from "./style";
 import monsterBurger from "../../../assets/icons/monster_burger.png";
 import { useNavigation } from "@react-navigation/native";
@@ -17,17 +17,15 @@ import { useSelector } from "react-redux";
 import { DrawerNavigationProp } from "@react-navigation/drawer";
 
 import { RootStackParamList } from "../RootStackPrams";
+import { Icon } from "@rneui/base";
 
 type authScreenProp = DrawerNavigationProp<RootStackParamList, "Register">;
 
 const Cardapio = () => {
-  const { user , token } = useSelector((state: any) => state.user);
+  const { user } = useSelector((state: any) => state.user);
   const navigation = useNavigation<authScreenProp>();
-  const { width, height } = useWindowDimensions();
 
-  const customUser = useSelector((state: any) => state.user.user);
-
-  console.log("o outro", customUser);
+  console.log(user.nome);
 
   const data = [
     {
@@ -48,67 +46,119 @@ const Cardapio = () => {
         " 2 Blends de carne de 150g, Queijo Cheddar, Bacon Caramelizado, Salada, Molho da casa,Pão brioche artesanal.",
       price: 25.25,
     },
+    {
+      name: "Burger",
+      description:
+        " 2 Blends de carne de 150g, Queijo Cheddar, Bacon Caramelizado, Salada, Molho da casa,Pão brioche artesanal.",
+      price: 25.25,
+    },
   ];
-
-  console.log(user.id);
 
   const openScreen = () => {
     navigation.navigate("Produtos");
   };
   return (
-    <View
-      style={[
-        styles.CardContainer,
-        {
-          flex: 1,
-          width,
-          height,
-          backgroundColor: Colors.Gray["0"],
-        },
-      ]}
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: Colors.Neutral.white,
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+      }}
     >
-      <View
-        style={{
-          position: "relative",
-          zIndex: 1,
-          width,
-          height: 150,
-          marginBottom: 15,
-        }}
-      >
-        <NavBar />
-        <Text
+      <View style={{ paddingHorizontal: 18, paddingVertical: 18 }}>
+        <View
           style={{
-            fontWeight: "bold",
-            color: Colors.Neutral.black,
-            textAlign: "center",
-            fontSize: 24,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            marginBottom: 4,
           }}
         >
-          Seja Bem-vindo {user.nome}👋
+          <Text
+            style={{
+              fontSize: 20,
+              lineHeight: 38.4,
+              color: Colors.Neutral.black,
+              fontWeight: "500",
+            }}
+          >
+            Olá, {user.nome} 👋🏾
+          </Text>
+          <TouchableOpacity
+            style={{
+              padding: 4,
+            }}
+            onPress={() => navigation.openDrawer()}
+          >
+            <Icon
+              type="material"
+              name="menu"
+              color={Colors.DeepYellow[6]}
+              size={24}
+            />
+          </TouchableOpacity>
+        </View>
+        <Text
+          style={{
+            fontSize: 14,
+            color: Colors.Gray[4],
+            fontWeight: "400",
+          }}
+        >
+          O que deseja pra hoje?
         </Text>
       </View>
 
-      <FlatList
-        data={data}
-        keyExtractor={(item) => item.name}
-        numColumns={2} // Número de colunas
-        renderItem={({ item }) => {
-          return (
-            <TouchableOpacity
-              onPress={openScreen}
-              style={[styles.cardProd, { width: width / 2 - 40 }]}
-            >
-              <Image style={{ width: 95, height: 95 }} source={monsterBurger} />
-              <View style={styles.text}>
-                <Text>{item.name}</Text>
-                <Text style={styles.total}>R${item.price}</Text>
-              </View>
-            </TouchableOpacity>
-          );
+      <View
+        style={{
+          flex: 1,
+          paddingHorizontal: 6,
         }}
-      />
-    </View>
+      >
+        <FlatList
+          data={data}
+          keyExtractor={(item) => item.name}
+          numColumns={2}
+          style={{
+            flex: 1,
+          }}
+          contentContainerStyle={{
+            flex: 1,
+          }}
+          renderItem={({ item }) => {
+            return (
+              <TouchableOpacity
+                onPress={openScreen}
+                style={[styles.cardProd, { flex: 2 }]}
+              >
+                <View
+                  style={{
+                    width: "100%",
+                    alignItems: "center",
+                  }}
+                >
+                  <Image
+                    style={{ width: 95, height: 95 }}
+                    source={monsterBurger}
+                  />
+                </View>
+                <View style={styles.text}>
+                  <Text
+                    style={{
+                      fontSize: 16,
+                      fontWeight: "600",
+                    }}
+                  >
+                    {item.name}
+                  </Text>
+                  <Text style={styles.total}>R${item.price}</Text>
+                </View>
+              </TouchableOpacity>
+            );
+          }}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
